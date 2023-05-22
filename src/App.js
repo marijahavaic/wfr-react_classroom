d3b23ade9ce9b880abd60fed70ef7ff36fc8d4f0
Marija Havaic <marija.havaic@gmail.com>
2023-05-22 09:55:53 +0200

    Add Header and Section to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div>
+            {/* Header */}
+            <Header />
+            {/* Reste de la page */}
+            <Section />
+        </div>
+    );
 }
 
 export default App;
